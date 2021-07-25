ad4d88270fa06a7c56c8007e6da202e6703b5a93
Saurabhrajaryan <saurabhrajaryan26@gmail.com>
2021-07-25 19:50:41 +0530

    Employee attendace check feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "welcome to employee wage computation problem"
+present=1
+absent=0
+check=$((RANDOM % 2))
+if [ $check -eq $present ]
+then
+	echo "Employee is present "
+else
+	echo "Employee is absent"
+fi
